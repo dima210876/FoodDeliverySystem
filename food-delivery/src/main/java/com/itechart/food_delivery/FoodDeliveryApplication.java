e014372c0fc3915c4e0a3f9709d69ea6a1bce33c
dima210876 <dima210876@mail.ru>
2022-02-25 06:03:39 +0300

    create project structure

---
+++ food-delivery/src/main/java/com/itechart/food_delivery/FoodDeliveryApplication.java
@@ -0,0 +1,15 @@
+package com.itechart.food_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class FoodDeliveryApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(FoodDeliveryApplication.class, args);
+    }
+}
