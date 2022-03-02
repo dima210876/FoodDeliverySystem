b7db7815fba6fb9bd39bbcbd3aef63875e79519f
dima210876 <dima210876@mail.ru>
2022-03-02 12:57:28 +0300

    add validation for dto

--- payment-service/src/main/java/com/itechart/payment_service/dto/PaymentAttemptDto.java
+++ payment-service/src/main/java/com/itechart/payment_service/dto/PaymentAttemptDto.java
@@ -1,17 +1,39 @@
 package com.itechart.payment_service.dto;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Data
 public class PaymentAttemptDto
 {
     private Long id;
+
+    @NotNull(message = "Payment receipt id is required")
     private PaymentReceiptDto paymentReceipt;
+
+    @NotNull(message = "Payment provider id is required")
     private PaymentProviderDto paymentProvider;
+
+    @Size(min = 10, max = 50, message = "Transaction number string length limits exceeded")
     private String transactionNumber;
+
+    @NotNull(message = "Payment method is required")
+    @NotBlank(message = "Payment method can't be empty")
+    @Size(min = 2, max = 50, message = "Payment method string length limits exceeded")
     private String paymentMethod;
+
+    @NotNull(message = "Payment status is required")
+    @NotBlank(message = "Payment status can't be empty")
+    @Size(min = 2, max = 20, message = "Payment status string length limits exceeded")
     private String paymentStatus;
+
+    @NotNull(message = "Payment datetime is required")
+    @NotBlank(message = "Payment datetime can't be empty")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime paymentDatetime;
 }
