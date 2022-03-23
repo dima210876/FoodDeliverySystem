851282a4c2aac9cfe15dc0305eb8d09d306ead1f
dima210876 <dima210876@mail.ru>
2022-03-23 18:56:55 +0300

    JL2-4 add validation exceptions handler

--- identity-service/src/main/java/com/itechart/identity_service/exception/ExceptionsHandler.java
+++ identity-service/src/main/java/com/itechart/identity_service/exception/ExceptionsHandler.java
@@ -2,8 +2,14 @@ package com.itechart.identity_service.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class ExceptionsHandler
@@ -12,5 +18,19 @@ public class ExceptionsHandler
     public ResponseEntity<String> handleException(EmailDuplicationException exception)
     {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex)
+    {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) ->
+        {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
