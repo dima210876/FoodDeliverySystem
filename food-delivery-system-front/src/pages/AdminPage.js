5f7e479e86ef6669085842ce12c45c729b84f912
Anton <antondanik32@mail.ru>
2022-03-23 16:19:35 +0300

    create admin page

---
+++ food-delivery-system-front/src/pages/AdminPage.js
@@ -0,0 +1,30 @@
+import {Link} from "react-router-dom";
+
+const AdminPage = () => {
+    return(
+        <div className='box'>
+            <div className='admin-image-div'>
+                <img className='admin-image' src='admin.png'/>
+            </div>
+            <div className='list-of-actions'>
+                <div className='title'>
+                    <h1>Administrator's space</h1>
+                </div>
+                <div className='actions'>
+                    <div className='link-div'>
+                        <Link className='link' to=''><h4>Restaurant manager registration</h4></Link>
+                    </div>
+                    <div className='link-div'>
+                        <Link className='link' to=''><h4>Courier organization manager registration</h4></Link>
+                    </div>
+                    <div className='link-div'>
+                        <Link className='link' to=''><h4>List of recent orders</h4></Link>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export {AdminPage}
