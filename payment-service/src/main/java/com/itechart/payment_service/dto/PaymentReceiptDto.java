2824a2de76491a9a194e5344d997497cc8cbca71
dima210876 <dima210876@mail.ru>
2022-03-24 18:54:40 +0300

    JL2-4 fix dto

--- payment-service/src/main/java/com/itechart/payment_service/dto/PaymentReceiptDto.java
+++ payment-service/src/main/java/com/itechart/payment_service/dto/PaymentReceiptDto.java
@@ -1,11 +1,17 @@
 package com.itechart.payment_service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.*;
 import java.util.Set;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PaymentReceiptDto
 {
     private Long id;
