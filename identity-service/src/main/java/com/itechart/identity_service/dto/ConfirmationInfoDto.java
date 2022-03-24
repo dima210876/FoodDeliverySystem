0671a3419130870fa66b4c1833874f2b425dfa14
dima210876 <dima210876@mail.ru>
2022-03-24 18:38:33 +0300

    JL2-4 fix email properties and dto for registration confirmation info

--- identity-service/src/main/java/com/itechart/identity_service/dto/ConfirmationInfoDto.java
+++ identity-service/src/main/java/com/itechart/identity_service/dto/ConfirmationInfoDto.java
@@ -1,13 +1,17 @@
 package com.itechart.identity_service.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ConfirmationInfoDto
 {
     @NotNull
