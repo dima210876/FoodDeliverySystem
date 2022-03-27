c7d66e7fb833c067cfe96de587c8b2c7d30cfd3b
Klepitskaya <jenka1330.gmail.com>
2022-03-27 18:44:48 +0300

    added validation to address field

--- food-delivery-system-front/src/components/inputFields/addressInputField.jsx
+++ food-delivery-system-front/src/components/inputFields/addressInputField.jsx
@@ -5,12 +5,8 @@ import PlacesAutocomplete, {
 } from "react-places-autocomplete";
 import {Form} from "react-bootstrap";
 
-const AddressInputField = ({ changeAddress, submitClicked  }) => {
+const AddressInputField = ({ changeAddress, changeCoordinates, submitClicked  }) => {
     const [inputAddress, setInputAddress] = useState("");
-    const [coordinates, setCoordinates] = useState({
-        lat: null,
-        lng: null
-    });
     const [error, setError] = useState("");
 
     useEffect(() => {
@@ -19,63 +15,58 @@ const AddressInputField = ({ changeAddress, submitClicked  }) => {
         }
     }, [submitClicked]);
 
-    function validateAddress(inputValue) {
-        if (inputValue) {
-            console.log('validating address:');
-
-            geocodeByAddress(inputValue)
-                .then(latLng => {
-                    setCoordinates(latLng);
-                    setError("");
-                })
-                .catch(error => {
+     function validateAddress(input) {
+         setInputAddress(input);
+         if (input) {
+              geocodeByAddress(input)
+                  .then(async results =>
+                  {
+                      const latLng = await getLatLng(results[0]);
+                      changeCoordinates(latLng);
+                      changeAddress(input);
+                  })
+                  .then(setError(""))
+                  .catch(error => {
                     setError(error);
-                    setCoordinates(null);
-                })
-            setInputAddress(inputValue);
+                    changeCoordinates({lat: null, lng: null});
+                    changeAddress("");
+                  })
         } else {
             setError("");
-        }
-        changeAddress(inputValue);
+            changeCoordinates({lat: null, lng: null});
+            changeAddress("");
+         }
     }
 
-    const handleSelect = value => {
-        validateAddress(value);
-    };
-
     return (
-            <PlacesAutocomplete
-                value={inputAddress}
-                onChange={setInputAddress}
-                onSelect={handleSelect}
-                // onSelect={setInputAddress}
+        <PlacesAutocomplete
+            value={inputAddress}
+            onChange={validateAddress}
+            onSelect={validateAddress}
+        >
+            {({ getInputProps, suggestions, getSuggestionItemProps }) => (
+                <Form.Group className="p-4 pt-0" controlId="address-input">
+                    <Form.Label>Office address</Form.Label>
+                    <input {...getInputProps({ placeholder: "Enter address", class: "form-control"} )} />
 
-            >
-                {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-                        <Form.Group className="p-4 pt-0" controlId="address-input">
-                            <Form.Label>Office address</Form.Label>
-                            <div>{inputAddress}</div>
-                            <input {...getInputProps({ placeholder: "Enter address", class: "form-control"} )} />
-
-                            <div className="form-control-feedback" style={{display: error !== '' ? 'block' : 'none' }}>
-                                { error === 'ZERO_RESULTS' ? 'There is no such address. Please select it from the suggestions.' : 'Address error'}
-                            </div>
-                        <div className="address-suggestions">
-                            {loading ? <div>...loading</div> : null}
-                            {suggestions.map(suggestion => {
-                                const style = {
-                                    backgroundColor: suggestion.active ? "#c8c8c8" : "#fff"
-                                };
-                                return (
-                                    <div {...getSuggestionItemProps(suggestion, { style })}>
-                                        {suggestion.description}
-                                    </div>
-                                );
-                            })}
-                        </div>
-                        </Form.Group>
-                )}
-            </PlacesAutocomplete>
+                    <div className="form-control-feedback" style={{display: error !== '' ? 'block' : 'none' }}>
+                        { error === 'ZERO_RESULTS' ? 'There is no such address. Please select it from the suggestions.' : 'Address error'}
+                    </div>
+                    <div className="address-suggestions">
+                        {suggestions.map(suggestion => {
+                            const style = {
+                                backgroundColor: suggestion.active ? "#c8c8c8" : "#fff"
+                            };
+                            return (
+                                <div {...getSuggestionItemProps(suggestion, { style })}>
+                                    {suggestion.description}
+                                </div>
+                            );
+                        })}
+                    </div>
+                </Form.Group>
+            )}
+        </PlacesAutocomplete>
     );
 }
 
