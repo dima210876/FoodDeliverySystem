0514240c615c6e81aad2716d42300f6b8de6746c
xwreix <anastasya.kovaliova2016>
2022-04-13 21:28:51 +0300

    fix changing status and getting list of orders

--- food-delivery/src/main/java/com/itechart/food_delivery/model/Order.java
+++ food-delivery/src/main/java/com/itechart/food_delivery/model/Order.java
@@ -21,21 +21,21 @@ public class Order {
     @Column(name = "courier_id")
     private Long courierId;
 
-//    @ManyToOne
-//    @JoinColumn(
-//            name = "customer_id",
-//            foreignKey = @ForeignKey(
-//                    name = "FK_customers_id"
-//            )
-//    )
-//
-//    @NotNull(message = "Customer is required")
-//    @JsonBackReference
-//    private Customer customer;
+    @ManyToOne
+    @JoinColumn(
+            name = "customer_id",
+            foreignKey = @ForeignKey(
+                    name = "FK_customers_id"
+            )
+    )
 
-    @Column(name = "customer_id")
-    @NotNull(message = "Customer ID is required")
-    private Long customerId;
+    @NotNull(message = "Customer is required")
+    @JsonBackReference
+    private Customer customer;
+
+//    @Column(name = "customer_id")
+//    @NotNull(message = "Customer ID is required")
+//    private Long customerId;
 
     @Column(name = "order_status")
     @NotNull(message = "Order status is required")
